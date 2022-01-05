d29b2d584f38262c3a7d7d70eb4336768a8bc4f8
harkouken <idir.harkouken@alumni.univ-avignon.fr>
2022-01-05 23:51:34 +0100

    Ajouts des fichiers contenant les mots de passe, et quelques mofications

--- src/password_generate.py
+++ src/password_generate.py
@@ -1,28 +1,29 @@
+# Load Larger LSTM network and generate text
 import sys
+from datetime import datetime
+
 import numpy
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.utils import np_utils
 
-# loader le corpus train
+# load ascii text and covert to lowercase
 filename = "../data/train.txt"
 raw_text = open(filename, 'r', encoding='utf-8').read()
-# traitement : miniscules
 raw_text = raw_text.lower()
-# associer les carachtères à leurs équivalents en int
+# create mapping of unique chars to integers, and a reverse mapping
 chars = sorted(list(set(raw_text)))
 char_to_int = dict((c, i) for i, c in enumerate(chars))
 int_to_char = dict((i, c) for i, c in enumerate(chars))
-
-# resumé du contenu du corpus
+# summarize the loaded data
 n_chars = len(raw_text)
 n_vocab = len(chars)
-print ("Nombre total des caractères: ", n_chars)
-print ("Total Vocabulaire: ", n_vocab)
+print ("Total Characters: ", n_chars)
 # prepare the dataset of input to output pairs encoded as integers
-seq_length = 10
+seq_length = 100
 dataX = []
 dataY = []
 for i in range(0, n_chars - seq_length, 1):
@@ -32,47 +33,54 @@ for i in range(0, n_chars - seq_length, 1):
 	dataY.append(char_to_int[seq_out])
 n_patterns = len(dataX)
 print ("Total Patterns: ", n_patterns)
-# reshape X à samples, time steps, features]
+# reshape X to be [samples, time steps, features]
 X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-# normalisation
+# normalize
 X = X / float(n_vocab)
+# one hot encode the output variable
 y = np_utils.to_categorical(dataY)
-# genrer le model
+# define the LSTM model
 model = Sequential()
 model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(128))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
-# upoloader le best model
+# phase de compilation
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# definir le checkpoint, point de repère pour un best modèle
+filepath = "../model/best.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+tensorboard = TensorBoard(log_dir="..\\logs\\fit\\"+datetime.now().strftime("%Y%m%d-%H%M%S"), histogram_freq=1, write_graph=True)
+callbacks_list = [checkpoint, tensorboard]
+
+# load the network weights
 filename = "../model/best.hdf5"
 model.load_weights(filename)
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accurancy')
-# on prends une seed aléatoirement
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+# pick a random seed
 start = numpy.random.randint(0, len(dataX)-1)
 pattern = dataX[start]
 print ("Seed:")
 print ("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
-# generate des mots de passe
+# generate mot de pass
+for i in range(500000):#nombre de mot de passe a renvoyé
+	x = numpy.reshape(pattern, (1, len(pattern), 1))
+	x = x / float(n_vocab)
+	prediction = model.predict(x, verbose=0)
+	index = numpy.argmax(prediction)
+	result = int_to_char[index]
+	seq_in = [int_to_char[value] for value in pattern]
+	sys.stdout.write(result)
+	pattern.append(index)
+	pattern = pattern[1:len(pattern)]
 
-for i in range(1000):
-  x = numpy.reshape(pattern, (1, len(pattern), 1))
-  x = x / float(n_vocab)
-  prediction = model.predict(x, verbose=0)
-  index = numpy.argmax(prediction)
-  result = int_to_char[index]
-  seq_in = [int_to_char[value] for value in pattern]
-  #sys.stdout.write(result)
+	file3 = open("mdp-générés3.txt", 'a+')
+	file3.write(str(result) )
 
-  pattern.append(index)
-  pattern = pattern[1:len(pattern)]
-  file3 = open("predicted_password_1000.txt", 'a+')
-  file3.write(str(result))
-    #print("Génération de 9000 mots de passe, sauvegardés dans:", "predicted_password_%d.txt" %pred_size )
 
-print("generating 1000 passwords: ")
-#gen_pred_pwd(1000)
-#print("generating 100000  passwords: ")
-#gen_pred_pwd(1000)
-#print("generating 1000000  passwords: ")
-#gen_pred_pwd(1000)
+
+print ("\nDone.")
+
+'''file3.write(result + "\n " + "\n")'''
